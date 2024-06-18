164645d3dc80b559fdf0478db89d29eeafaf078c
ejayawan22 <ejayawan22@gmail.com>
2024-06-18 18:01:32 +0800

    improve folder structure

---
+++ frontend/src/layouts/PageContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageContainer = ({ children }) => {
+  return (
+    <div className='px-20'>{children}</div>
+  )
+}
+
+export default PageContainer
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageContainer from '../layouts/PageContainer'
+
+const Home = () => {
+  return (
+    <div>
+        <PageContainer>
+          Home
+        </PageContainer>
+    </div>
+  )
+}
+
+export default Home
